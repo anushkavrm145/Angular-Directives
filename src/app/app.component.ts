bfce0f632e184c8507d24f27d0a121df2bc7a44b
anushkavrm145 <anushkavrm145@gmail.com>
2024-09-04 18:29:22 +0530

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngDirectives';
+  postArray:Array<string>=['Post 1','Post 2','Post 3','Post 4','Post 5']
+  objArray: Array<any>=[
+    {id:1,postTitle:'Post 1'},
+    {id:2,postTitle:'Post 2'},
+    {id:3,postTitle:'Post 3'},
+    {id:4,postTitle:'Post 4'},
+    {id:5,postTitle:'Post 5'},
+
+
+  ]
+  stepForm:string='hi'
+  constructor(){
+    for(let i=0;i<this.postArray.length;i++){
+      console.log(this.postArray[i])
+    }
+  }
+  isActive:boolean=false;
+  addnew(){
+    this.objArray.push({id:6,postTitle:"Post 6"})
+  }
+  onDelete(index:number){
+    this.objArray.splice(index,1);
+  }
+  onClick(status:string){
+    this.stepForm=status
+  }
 }
